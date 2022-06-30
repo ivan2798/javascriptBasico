eaa65c009db4ed01562199abff26638a0eafa159
ivan2798 <inge1998@hotmail.com>
2022-06-30 17:36:55 -0500

    Ahora tipos de datos numeric, bool, string git status

--- app.js
+++ app.js
@@ -3,9 +3,17 @@
 
 //keyword: var, let, const
 
+//dato primitivo: 
+
+let numero = 3;
+numero = 4;
+
+let completo = true;
+
 const nombre = "Ivan";
 
 let apellido = "Herrera";
 
 apellido = "Barrios";
-console.log(nombre);
+
+console.log(nombre,completo);
