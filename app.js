07493cdbfb082b23cbd80fdaa2ae302c2e5c4e34
ivan2798 <inge1998@hotmail.com>
2022-06-30 17:45:45 -0500

    Ahora nuevos operadores

--- app.js
+++ app.js
@@ -9,5 +9,20 @@
 
 let num1 = 2;
 let num2="2";
+
 console.log(num1==num2); // == no importan tipos
 console.log(num1===num2);  // debe ser mismo tipo
+
+let x = 5;
+x = x + 5;
+
+console.log("El valor de x es: ",x);
+
+let vehiculos = 40;
+let vehiculosBaja = 10;
+let vehiculosElectricos = 15;
+vehiculos-=vehiculosBaja;
+vehiculos+=vehiculosElectricos;
+
+
+console.log("La policia tiene ",vehiculos, "en total");
